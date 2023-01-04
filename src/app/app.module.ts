4d43a231f62b4b340acbcf8db13d83a89d298f70
Javier Rodriguez Marulanda <rodriguez.javier.6170@eam.edu.co>
2023-01-03 21:54:25 -0500

    vs1 - subida de archivos principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AcercaDeMiComponent
   ],
   imports: [
     BrowserModule,
